8e26b20f3d952f7442521c3c700a7c410fed67b9
Naan Ahmed <naanahmed.na@gmail.com>
2022-10-10 12:32:42 +0300

    Make frontend and Reverseproxy loadbalancers

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a445ad8a536f04bb0b5baa7fd1d14028-208018871.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
